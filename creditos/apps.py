5af96b83a9f72a1dbfab5ca3d8fd3836fd6ac51b
EDUARDO PEREZ <peduar7@gmail.com>
2025-08-29 19:54:48 -0600

    Add files via upload

---
+++ creditos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+class CreditosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'creditos'
+    verbose_name = 'Módulo Créditos'
---
+++ informacion/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+class InformacionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'informacion'
+    verbose_name = 'Módulo Información'
